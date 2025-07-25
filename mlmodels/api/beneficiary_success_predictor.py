7bf0e2f1607d07db0c0800ac80bb39aea85b09d8
Rashmi Rajeshirke <rashmi.rajeshirke.2003@gmail.com>
2025-07-25 09:21:08 +0530

    Update scoring method and removed unused params

--- mlmodels/api/beneficiary_success_predictor.py
+++ mlmodels/api/beneficiary_success_predictor.py
@@ -8,18 +8,20 @@ app = FastAPI(title="Success Score Predictor")
 model = joblib.load("../xgb_pipeline.pkl")
 
 FEATURE_ORDER = [
-    "age", "sex", "race_ethnicity", "marital_status", "education_level",
+    "age", "sex", "marital_status", "education_level",
     "income_level", "occupation", "place_of_residence", "nationality",
     "religion", "language_spoken", "household_size", "credit_score",
-    "has_outstanding_loans", "num_loans_taken_last_5yrs", "loan_repayment_history_score",
-    "monthly_expenses", "existing_monthly_liabilities", "bank_account_access",
-    "internet_access", "distance_to_nearest_bank_km", "mobile_money_usage"
+    "has_outstanding_loans", "num_loans_taken_last_5yrs", "bank_account_access",
+    "internet_access", "distance_to_nearest_bank_km", "mobile_money_usage", 
+    "employment_status", "has_active_savings_account", "income_growth_by_15%_in_6months",
+    "loan_used_for_successful_self_employment", "loan_used_for_child_education",
+    "past_funding_recieved", "willing_to_participate_in_CSR", 
+    "willing_to_educate_girl_child", "willing_to_give_children_higher_education"
 ]
 
 class BeneficiaryInput(BaseModel):
     age: int
     sex: str
-    race_ethnicity: str
     marital_status: str
     education_level: str
     income_level: int
@@ -32,21 +34,35 @@ class BeneficiaryInput(BaseModel):
     credit_score: int
     has_outstanding_loans: int
     num_loans_taken_last_5yrs: int
-    loan_repayment_history_score: float
-    monthly_expenses: int
-    existing_monthly_liabilities: int
-    bank_account_access: int
-    internet_access: int
-    distance_to_nearest_bank_km: float
-    mobile_money_usage: int
+    # bank_account_access: int
+    # internet_access: int
+    # distance_to_nearest_bank_km: float
+    # mobile_money_usage: int
+    employment_status: str
+    has_active_savings_account: int
+    income_growth_by_15percent_in_6months:int 
+    loan_used_for_successful_self_employment: int
+    loan_used_for_child_education: int
+    past_funding_recieved: int
+    willing_to_participate_in_CSR: int
+    willing_to_educate_girl_child: int
+    willing_to_give_children_higher_education:int
+
+
 
 def calculate_base_score(data: dict) -> float:
+    """Calculate base score on the basis of adjustable demographics"""
+    # to do : change the demographic
+    # make them adjustable using some stored value
+    # add apt loan usage to this itself
+    
     score = 0.0
-    score += 0.08 if data["sex"] == "Female" else 0
-    score += 0.05 if data["education_level"] in {"Graduate", "Postgraduate"} else 0
-    score += 0.05 if data["income_level"] >= 50000 else 0
-    score += 0.03 if data["place_of_residence"] == "Urban" else 0
-    score += 0.02 if data["household_size"] <= 3 else -0.01
+    score += 0.3 if data["sex"] == "Female" else 0
+    score += 0.3 if data["place_of_residence"] == "Rural" else 0
+    score -= 0.8 * data["past_funding_recieved"]
+    score += 0.2 * data["willing_to_participate_in_CSR"] 
+    score += 0.2 * data["willing_to_educate_girl_child"]
+    score += 0.15 * data["willing_to_give_children_higher_education"]
     return score
 
 def prepare_model_input(data: dict) -> pd.DataFrame:
